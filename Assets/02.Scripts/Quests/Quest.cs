ef73df5f74405bb0c3ba85c8879cff93e96e1249
KUROJIWOON <114848984+DINNYARING@users.noreply.github.com>
2022-12-22 15:43:12 +0900

    Quest.cs 생성 및 작성
    Quest.cs 생성 및 작성

---
+++ Assets/02.Scripts/Quests/Quest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Quest
+{
+    public enum QuestProgress { NOT_AVAILABLE, AVAILABLE, ACCEPTED, COMPLETE, DONE }
+
+    public string title;            // title of the quest
+    public int id;                  // ID number for the quest
+    public QuestProgress progress;  // state of the current quest (enum)
+    public string description;      // string from our quest Giver/Receiver
+    public string hint;             // string from our quest Giver/Receiver
+    public string congratulation;   // string from our quest Giver/Receiver
+    public string summary;          // string from our quest Giver/Receiver
+    public int nextQuest;           // the next quest, if there is any (chain quest)
+
+    public string questObjective;   // name of the quest objective(also for remove monsters)
+    public int questObjectiveCount; // current number of quest objective count
+    public int questObjectiveRequirement;   // required amount of quest objective objects
+
+    // REWARD
+    public int expReward;
+    public int goldReward;
+    public string itemReward;
+}
